3dcf536ad09cc5b4eaddc57ddfbea44abd8abf4d
Gregory Zentkovich <gzentkovich@gmail.com>
2020-10-22 07:31:20 -1000

    added card contents

---
+++ app.js
@@ -0,0 +1,11 @@
+const vm = new Vue({
+  el: '#app',
+  data: {
+    results: [
+      {title: "post number one.", abstract: "lorem ipsum some test dimpsum"},
+      {title: "post number two.", abstract: "lorem ipsum some test dimpsum"},
+      {title: "post number three.", abstract: "lorem ipsum some test dimpsum"},
+      {title: "post number four.", abstract: "lorem ipsum some test dimpsum"}
+    ]
+  }
+});
